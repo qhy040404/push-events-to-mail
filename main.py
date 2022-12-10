83c75d0dd2c575e10d1879d05f66b546e52bf8f3
qhy040404 <qhy040404@163.com>
2022-12-10 12:54:05 +0800

    check if outdated

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import datetime
 import json
 
 import requests
@@ -19,13 +20,18 @@ columns = ['Actor', 'Repo', 'Time']
 src = s.get('https://api.github.com/users/qhy040404/received_events', verify=False).text
 src_json: dict = json.loads(src)
 
+outdated_time = datetime.datetime.utcnow() - datetime.timedelta(days=1)
+
 event: dict
 for event in src_json:
     if event['type'] == 'WatchEvent':
-        target_events.append([
-            event['actor']['login'],
-            event['repo']['name'],
-            event['created_at']
-        ])
+        latest = datetime.datetime.strptime(event['created_at'], '%Y-%m-%dT%H:%M:%SZ')
+        if latest > outdated_time:
+            target_events.append([
+                event['actor']['login'],
+                event['repo']['name'],
+                event['created_at']
+            ])
 
-em.send_email(target_events, columns)
+if len(target_events) > 0:
+    em.send_email(target_events, columns)
